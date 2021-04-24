050267663d78137ac73a5757b9fc149bc43b138a
Eoin Motherway <eoin@yukitsune.dev>
2021-04-24 11:53:46 +1000

    Started copying Guerrilla

--- cmd/chameleon-test/main.go
+++ cmd/chameleon-test/main.go
@@ -1,1 +1,36 @@
-package chameleon_test
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/smtp"
+)
+
+func main() {
+
+	fmt.Println("Hello, World!")
+
+	host := "127.0.0.1:2525"
+	client, err := smtp.Dial(host)
+	if err != nil {
+		return
+	}
+
+	err = client.Rcpt("test@test.com")
+	if err != nil {
+		return
+	}
+
+	auth := smtp.PlainAuth("test_id", "test_username", "test_password", host)
+
+	to := []string{"recipient@example.net"}
+	msg := []byte("To: recipient@example.net\r\n" +
+		"Subject: discount Gophers!\r\n" +
+		"\r\n" +
+		"This is the email body.\r\n")
+
+	err = smtp.SendMail(host, auth, "sender@example.org", to, msg)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- cmd/chameleon/main.go
+++ cmd/chameleon/main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"sync"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	WaitForCtrlC()
+}
+
+
+func WaitForCtrlC() {
+	var endWaiter sync.WaitGroup
+	endWaiter.Add(1)
+
+	var signalChannel chan os.Signal
+	signalChannel = make(chan os.Signal, 1)
+	signal.Notify(signalChannel, os.Interrupt)
+	go func() {
+		<-signalChannel
+		endWaiter.Done()
+	}()
+	endWaiter.Wait()
 }
