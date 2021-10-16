4f36bc1ad56621942078502f847a20eb05c74eb9
Eoin Motherway <25342760+YuKitsune@users.noreply.github.com>
2021-10-16 10:59:07 +1000

    Add tailwind

---
+++ public/frontend/tailwind.config.cjs
@@ -0,0 +1,10 @@
+// tailwind.config.cjs
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+	purge: ['./src/**/*.svelte', './src/**/*.css'],
+	darkMode: 'media',
+	theme: {},
+	variants: {},
+	plugins: [],
+}
