cad79bec7b038d6f8ebc5643b2e34b5ec2923e18
Eoin Motherway <25342760+YuKitsune@users.noreply.github.com>
2021-10-20 20:13:05 +1000

    Start scoping out account and settings pages

---
+++ public/frontend/src/routes/account.svelte
@@ -0,0 +1,30 @@
+
+<h1 class='text-3xl'>My Details</h1>
+<!-- Primary/secondary email address -->
+
+<h1 class='text-3xl'>Billing</h1>
+
+<h1 class='text-xl'>Tiers</h1>
+
+<h1 class='text-xl'>Storage</h1>
+
+<h1 class='text-xl'>Payment Method</h1>
+
+<h1 class='text-3xl'>Security</h1>
+<h1 class='text-xl'>Password</h1>
+<!-- Update password -->
+
+<h1 class='text-xl'>Two-Factor Authentication</h1>
+<!-- TOTP -->
+<!-- U2F -->
+
+<h1 class='text-xl'>Events</h1>
+<!-- New logins -->
+<!-- New logins -->
+
+<h1 class='text-xl'>Sessions</h1>
+<!-- Signed in devices -->
+<!-- Signed out of all devices -->
+
+<h1 class='text-3xl'>Danger Zone</h1>
+<!-- Delete account -->
---
+++ public/frontend/src/routes/settings.svelte
@@ -0,0 +1,11 @@
+
+<h1 class='text-3xl'>Mail Forwarding</h1>
+<!-- Retry interval -->
+<!-- Number of retries before giving up -->
+
+<h1 class='text-3xl'>Quarantined Mail</h1>
+<!-- Days to keep before deleting -->
+
+<h1 class='text-3xl'>Danger Zone</h1>
+<!-- Delete all quarantined mail -->
+<!-- Delete all aliases -->
