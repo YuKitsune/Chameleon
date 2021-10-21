fef5d4056c75fa1b64ab0476ebe5f9c369907807
Eoin Motherway <25342760+YuKitsune@users.noreply.github.com>
2021-10-21 17:21:29 +1000

    Make dropdown hide on block

--- public/frontend/src/lib/components/Dropdown.svelte
+++ public/frontend/src/lib/components/Dropdown.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	export let isOpen = false;
-	const toggleIsOpen = () => isOpen = !isOpen;
+	const toggleDropdown = () => isOpen = !isOpen;
+
+	const hideDropdown = () => {
+		isOpen = false;
+	}
 
 	let dropdownElement;
 	function isInDropdown(target) {
@@ -19,18 +23,18 @@
 		if (isInDropdown(event.target))
 			return;
 
-		toggleIsOpen();
+		hideDropdown();
 	}
 </script>
 
 <svelte:body on:click={onClickOutside} />
 
 <div class="relative" bind:this={dropdownElement}>
-	<div on:click={toggleIsOpen}>
+	<div on:click={toggleDropdown}>
 		<slot name='button' />
 	</div>
 	{#if isOpen}
-		<div class='absolute right-0 mt-2 py-2 w-40 bg-white rounded-md shadow-md'>
+		<div class='absolute right-0 mt-2 py-2 w-40 bg-white rounded-md shadow-md' on:click={hideDropdown}>
 			<slot name='items' />
 		</div>
 	{/if}
